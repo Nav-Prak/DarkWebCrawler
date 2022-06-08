96a04d50738d6c8968105c45775bafda9864f430
navprak <naveenprakaasam@gmail.com>
2022-06-08 14:11:17 +0530

    Created new template

--- app.py
+++ app.py
@@ -34,3 +34,22 @@ def analyse():
 	#f.close()
 
 	return render_template('results.html', name=name)
+
+@app.route('/<path:req_path>')
+def dir_listing(req_path):
+    BASE_DIR = script_dir
+
+    # Joining the base and the requested path
+    abs_path = os.path.join(BASE_DIR, req_path)
+
+    # Return 404 if path doesn't exist
+    if not os.path.exists(abs_path):
+        return abort(404)
+
+    # Check if path is a file and serve
+    if os.path.isfile(abs_path):
+        return send_file(abs_path)
+
+    # Show directory contents
+    files = os.listdir(abs_path)
+    return render_template('files.html', files=files)
